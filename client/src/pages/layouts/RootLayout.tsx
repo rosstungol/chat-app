0dde71e5ad4bf64b62e1906d1c8fa2b377c3d607
Ross Tungol <rossjstungol@gmail.com>
2024-07-27 18:22:24 +0800

    Implements basic Stream Homepage

--- client/src/pages/layouts/RootLayout.tsx
+++ client/src/pages/layouts/RootLayout.tsx
@@ -1,5 +1,9 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuth } from '../../context/hooks'
 
 export function RootLayout() {
+	const { user } = useAuth()
+	if (user == null) return <Navigate to="/login" />
+
 	return <Outlet />
 }
