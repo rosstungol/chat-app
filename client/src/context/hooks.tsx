5a47185d1b535e2be81a9844e60f9fef7f8909b0
Ross Tungol <rossjstungol@gmail.com>
2024-07-27 18:21:41 +0800

    Adds useLoggedInAuth context hook

--- client/src/context/hooks.tsx
+++ client/src/context/hooks.tsx
@@ -4,3 +4,8 @@ import { Context, AuthContext } from './AuthContext'
 export function useAuth() {
 	return useContext(Context) as AuthContext
 }
+
+export function useLoggedInAuth() {
+	return useContext(Context) as AuthContext &
+		Required<Pick<AuthContext, 'user'>>
+}
