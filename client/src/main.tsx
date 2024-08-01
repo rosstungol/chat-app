05d85d048a4453d0fbfd96ab9415f4d65fe8141d
Ross Tungol <rossjstungol@gmail.com>
2024-08-02 00:11:51 +0800

    Updates Home page styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import 'stream-chat-react/dist/css/index.css'
+import 'stream-chat-react/dist/css/v2/index.css'
 
 const queryClient = new QueryClient()
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,6 +7,7 @@ import {
 	LoadingIndicator,
 	MessageInput,
 	MessageList,
+	Thread,
 	useChatContext,
 	Window,
 } from 'stream-chat-react'
@@ -21,18 +22,23 @@ export function Home() {
 
 	return (
 		<Chat client={streamChat}>
-			<ChannelList
-				List={Channels}
-				sendChannelsToList
-				filters={{ members: { $in: [user.id] } }}
-			/>
-			<Channel>
-				<Window>
-					<ChannelHeader />
-					<MessageList />
-					<MessageInput />
-				</Window>
-			</Channel>
+			<main className="flex h-screen">
+				<ChannelList
+					List={Channels}
+					sendChannelsToList
+					filters={{ members: { $in: [user.id] } }}
+				/>
+				<section className="flex-auto p-3">
+					<Channel>
+						<Window>
+							<ChannelHeader />
+							<MessageList />
+							<MessageInput />
+						</Window>
+						<Thread />
+					</Channel>
+				</section>
+			</main>
 		</Chat>
 	)
 }
@@ -43,39 +49,49 @@ function Channels({ loadedChannels }: ChannelListMessengerProps) {
 	const { setActiveChannel, channel: activeChannel } = useChatContext()
 
 	return (
-		<div className="m-3 flex h-full w-60 flex-col gap-4">
-			<Button onClick={() => navigate('/channel/new')}>New Conversation</Button>
-			<hr className="border-slate-400" />
-			{loadedChannels != null && loadedChannels.length > 0
-				? loadedChannels.map((channel) => {
-						const isActive = channel === activeChannel
-						const extraClasses = isActive
-							? 'bg-blue-500 text-white'
-							: 'hover:bg-blue-100 bg-slate-100'
-						return (
-							<button
-								onClick={() => setActiveChannel(channel)}
-								disabled={isActive}
-								className={`flex items-center gap-3 rounded-lg p-4 ${extraClasses}`}
-								key={channel.id}
-							>
-								{channel.data?.image && (
-									<img
-										src={channel.data.image}
-										className="h-10 w-10 rounded-full object-cover object-center"
-									/>
-								)}
-								<div className="overflow-hidden text-ellipsis whitespace-nowrap">
-									{channel.data?.name || channel.id}
-								</div>
-							</button>
-						)
-					})
-				: 'No Conversations'}
-			<hr className="mt-auto border-slate-400" />
-			<Button onClick={() => logout.mutate()} disabled={logout.isPending}>
-				Log out
-			</Button>
+		<div className="flex h-full w-72 flex-col bg-white">
+			<div className="border-b border-gray-200 p-4">
+				<Button onClick={() => navigate('/channel/new')} variant="primary">
+					New Conversation
+				</Button>
+			</div>
+			<div className="h-full space-y-2 border-b border-gray-200 px-4 py-4">
+				{loadedChannels != null && loadedChannels.length > 0
+					? loadedChannels.map((channel) => {
+							const isActive = channel === activeChannel
+							const extraClasses = isActive
+								? 'bg-blue-50'
+								: 'hover:bg-slate-50 bg-white'
+							return (
+								<button
+									onClick={() => setActiveChannel(channel)}
+									disabled={isActive}
+									className={`flex w-full items-center gap-3 rounded p-4 hover:cursor-pointer ${extraClasses}`}
+									key={channel.id}
+								>
+									{channel.data?.image && (
+										<img
+											src={channel.data.image}
+											className="h-10 w-10 rounded-full object-cover object-center"
+										/>
+									)}
+									<div className="overflow-hidden text-ellipsis whitespace-nowrap">
+										{channel.data?.name || channel.id}
+									</div>
+								</button>
+							)
+						})
+					: 'No Conversations'}
+			</div>
+			<div className="p-4">
+				<Button
+					variant="secondary"
+					onClick={() => logout.mutate()}
+					disabled={logout.isPending}
+				>
+					Log out
+				</Button>
+			</div>
 		</div>
 	)
 }
