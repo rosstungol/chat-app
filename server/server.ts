5a6914e7276edee16759e4fb47832869d0781dc1
Ross Tungol <rossjstungol@gmail.com>
2024-08-03 14:05:56 +0800

    Updates server.ts

--- server/server.ts
+++ server/server.ts
@@ -10,3 +10,29 @@ app.register(cors, { origin: process.env.CLIENT_URL })
 app.register(userRoutes)
 
 app.listen({ port: parseInt(process.env.PORT!) })
+
+app.get("/", async (req, reply) => {
+  return reply.status(200).type("text/html").send(html)
+})
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
+    />
+    <title>Vercel + Fastify Hello World</title>
+    <meta
+      name="description"
+      content="This is a starter template for Vercel + Fastify."
+    />
+  </head>
+  <body>
+    <p>Hello</p>
+  </body>
+</html>
+`
