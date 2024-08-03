3854a594a78a1d778f7b29364a2ad311f05ee6ae
Ross Tungol <rossjstungol@gmail.com>
2024-08-03 14:07:25 +0800

    Updates Home get response

--- server/server.ts
+++ server/server.ts
@@ -21,18 +21,10 @@ const html = `
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
-    />
-    <title>Vercel + Fastify Hello World</title>
-    <meta
-      name="description"
-      content="This is a starter template for Vercel + Fastify."
-    />
+    <title>Chat app server</title>
   </head>
   <body>
-    <p>Hello</p>
+    <p>Chat app server</p>
   </body>
 </html>
 `
