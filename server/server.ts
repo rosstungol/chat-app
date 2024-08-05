157e58455cb6b4cfb780252957eedd839bbfef52
Ross Tungol <rossjstungol@gmail.com>
2024-08-05 16:32:20 +0800

    Removes root route response

--- server/server.ts
+++ server/server.ts
@@ -10,21 +10,3 @@ app.register(cors, { origin: process.env.CLIENT_URL })
 app.register(userRoutes)
 
 app.listen({ port: parseInt(process.env.PORT!) })
-
-app.get("/", async (req, reply) => {
-  return reply.status(200).type("text/html").send(html)
-})
-
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Chat app server</title>
-  </head>
-  <body>
-    <p>Chat app server</p>
-  </body>
-</html>
-`
