a443bc355e6e7c6c4763250ac9a7510653a994b8
Ross Tungol <rossjstungol@gmail.com>
2024-08-07 01:34:43 +0800

    Updates server config

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,9 @@ config()
 
 import fastify from "fastify"
 import cors from "@fastify/cors"
-import { userRoutes } from "./routes/users"
+import { userRoutes } from "./api/users"
 
-const app = fastify()
+const app = fastify({ logger: true })
 app.register(cors, { origin: process.env.CLIENT_URL })
 app.register(userRoutes)
 
